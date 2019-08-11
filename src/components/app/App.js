7c7aa996006fc4215dbde8eba869ef330b820cde
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-08-11 16:45:57 +0300

    feat: add app logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Layout from '../layout/layout';
+import List from '../list/list';
+import Send from '../send/send';
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: [],
+      open: false,
+      connected: false,
+    }
+    this.socket = new WebSocket('ws://st-chat.shas.tel');
+    this.socket.onopen = () => {
+      this.setState({
+        connected: true,
+      })
+    }
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+
+  sendMessage(message) {
+    if (this.state.connected) {
+      this.socket.send(JSON.stringify({ from: 'pampers', message: `${message}` }))
+    }
+  }
+
+  componentDidMount() {
+    this.socket.onmessage = (e) => {
+      this.setState({
+        messages: JSON.parse(e.data).concat(this.state.messages)
+      })
+    }
+  }
+
+  render() {
+    console.log(this.state.messages[0])
+    return (
+      <Layout>
+        <List messages={this.state.messages} />
+        <Send sendMessage={this.sendMessage} />
+      </Layout>
+    );
+  }
 }
 
 export default App;
