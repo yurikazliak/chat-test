ed42a46bfea98c7f775dddc76dd6349bc4908e01
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-08-13 10:38:42 +0300

    feat: add autoscroll to bottom

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,41 +1,51 @@
 import React from 'react';
 import { DateTime } from "luxon";
 import { connect } from 'react-redux';
+import { css } from 'glamor';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 import listStyles from './list.module.scss';
 
 const List = ({ messages }) => {
-  console.log('messages', messages)
+  // console.log('messages', messages)
+
+  const ROOT_CSS = css({
+    height: '100%',
+    width: '100%'
+  });
+
   return (
     <ul className={listStyles.list}>
-      {messages.sort((a, b) => {
-        return a.time < b.time ? -1 : 1;
-      }).map((message, i) => {
-        return (
-          <li
-            key={message.id}
-            className={i === messages.length - 1 ? listStyles.messageNew : listStyles.messageBlock}
-          >
-            <div className={listStyles.from}>
-              {message.from}
-            </div>
-            <span className={listStyles.time}>
-              <>
-                {DateTime.fromMillis(message.time).c.year}-
+      <ScrollToBottom className={ROOT_CSS}>
+        {messages.sort((a, b) => {
+          return a.time < b.time ? -1 : 1;
+        }).map((message, i) => {
+          return (
+            <li
+              key={message.id}
+              className={i === messages.length - 1 ? listStyles.messageNew : listStyles.messageBlock}
+            >
+              <div className={listStyles.from}>
+                {message.from}
+              </div>
+              <span className={listStyles.time}>
+                <>
+                  {DateTime.fromMillis(message.time).c.year}-
               {DateTime.fromMillis(message.time).c.month}-
               {DateTime.fromMillis(message.time).c.day}
-                {'   '}
-                {DateTime.fromMillis(message.time).c.hour}-
+                  {'   '}
+                  {DateTime.fromMillis(message.time).c.hour}-
               {DateTime.fromMillis(message.time).c.minute}-
               {DateTime.fromMillis(message.time).c.second}
-              </>
-            </span>
-            <div className={listStyles.text}>
-              {message.message}
-            </div>
-          </li>
-        )
-      })}
+                </>
+              </span>
+              <div className={listStyles.text}>
+                {message.message}
+              </div>
+            </li>
+          )
+        })}
+      </ScrollToBottom>
     </ul>
   )
 }
